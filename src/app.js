16d697a3d3b8cb1fd6b61cd81965283d9b61c046
gautam <gautampal560@gmail.com>
2022-10-16 13:46:17 +0530

    Commit Message

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-
-const express = require('express');
-const app = express()
-
+const express = require("express");
+const app = express();
 
 // Your code goes here
 
+const Subscriber = require("./models/subscribers");
 
+app.get("/subscribers", (req, res) => {
+  Subscriber.find().then((subscriber) => res.send(subscriber));
+});
 
+app.get("/subscribers/names", (req, res) => {
+  Subscriber.find()
+    .select({ name: 1, subscribedChannel: 1 })
+    .then((subscriber) => res.send(subscriber));
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/subscribers/:id", (req, res) => {
+  const id = req.params.id;
+  Subscriber.find({ _id: id })
+    .then((subscribers) =>
+      subscribers.map((subscriber) => res.send(subscriber))
+    )
+    .catch((error) => res.status(400).send({ message: error.message }));
+});
 
 module.exports = app;
